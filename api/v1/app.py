75c21cd4415b48ea07975f34e787e61d63270556
Ibrahim Tijani <teebabs52@gmail.com>
2025-02-16 15:53:30 +0000

    Updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Flask App for AirBnB Clone API"""
-from flask import Flask, jsonify
+"""app.py to connect to API"""
+import os
 from models import storage
 from api.v1.views import app_views
-import os
+from flask import Flask, Blueprint, jsonify, make_response
+from flask_cors import CORS
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
+
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """Closes the storage on teardown"""
+def teardown_appcontext(code):
+    """teardown_appcontext"""
     storage.close()
 
+
 @app.errorhandler(404)
-def not_found(error):
-    """Handles 404 errors and returns JSON response"""
-    return jsonify({"error": "Not found"}), 404
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
-    app.run(host=host, port=port, threaded=True)
-
+    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=int(os.getenv('HBNB_API_PORT', '5000')))
