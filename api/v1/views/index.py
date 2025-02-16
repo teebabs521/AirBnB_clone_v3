9ea28b21e3237c59992eb27f280d0920b9940d39
Ibrahim Tijani <teebabs52@gmail.com>
2025-02-16 14:21:39 +0000

    Updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,24 @@
 """Index file for API routes"""
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
     """Returns API status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Returns the number of each object type"""
+    classes = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    stats = {key: storage.count(value) for key, value in classes.items()}
+    return jsonify(stats)
 
