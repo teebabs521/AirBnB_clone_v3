f58e7ef219561dc13279a90b44074a2a50d393b3
Ibrahim Tijani <teebabs52@gmail.com>
2025-02-16 16:04:12 +0000

    Updates

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,76 +1,57 @@
- #!/usr/bin/python3
-"""Amenities"""
+#!/usr/bin/python3
+"""
+    Flask route that returns json respone
+"""
 from api.v1.views import app_views
-from flask import request, jsonify, abort
-from models import storage, amenity
+from flask import abort, jsonify, request
+from models import storage, CNC
+from flasgger.utils import swag_from
 
 
-@app_views.route('/amenities', methods=['GET'], strict_slashes=False)
-def getallamenities():
-    """Gets all amenities"""
-    res = []
-    for i in storage.all("Amenity").values():
-        res.append(i.to_dict())
+@app_views.route('/amenities/', methods=['GET', 'POST'])
+@swag_from('swagger_yaml/amenities_no_id.yml', methods=['GET', 'POST'])
+def amenities_no_id(amenity_id=None):
+    """
+        amenities route that handles http requests no ID given
+    """
+    if request.method == 'GET':
+        all_amenities = storage.all('Amenity')
+        all_amenities = [obj.to_json() for obj in all_amenities.values()]
+        return jsonify(all_amenities)
 
-    return jsonify(res)
+    if request.method == 'POST':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        if req_json.get('name') is None:
+            abort(400, 'Missing name')
+        Amenity = CNC.get('Amenity')
+        new_object = Amenity(**req_json)
+        new_object.save()
+        return jsonify(new_object.to_json()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'],
-                 strict_slashes=False)
-def getamenity(amenity_id=None):
-    """Gets an amenity"""
-    s = storage.get("Amenity", amenity_id)
-    if s is None:
-        abort(404)
-    else:
-        return jsonify(s.to_dict())
+@app_views.route('/amenities/<amenity_id>', methods=['GET', 'DELETE', 'PUT'])
+@swag_from('swagger_yaml/amenities_id.yml', methods=['GET', 'DELETE', 'PUT'])
+def amenities_with_id(amenity_id=None):
+    """
+        amenities route that handles http requests with ID given
+    """
+    amenity_obj = storage.get('Amenity', amenity_id)
+    if amenity_obj is None:
+        abort(404, 'Not found')
 
+    if request.method == 'GET':
+        return jsonify(amenity_obj.to_json())
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
-                 strict_slashes=False)
-def deleteamenity(amenity_id=None):
-    """Deletes an amenity"""
-    s = storage.get("Amenity", amenity_id)
-    if s is None:
-        abort(404)
-    else:
-        storage.delete(obj)
-        storage.save()
+    if request.method == 'DELETE':
+        amenity_obj.delete()
+        del amenity_obj
         return jsonify({}), 200
 
-
-@app_views.route('/amenities', methods=['POST'], strict_slashes=False)
-def createamenity():
-    """Create an amenity"""
-    s = request.get_json(silent=True)
-    if s is None:
-        abort(400, "Not a JSON")
-    elif "name" not in s.keys():
-        abort(400, "Missing name")
-    else:
-        new_s = amenities.Amenity(**s)
-        storage.new(new_s)
-        storage.save()
-        return jsonify(new_s.to_dict()), 201
-
-
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
-                 strict_slashes=False)
-def updateamenity(amenity_id=None):
-    """Update an amenity"""
-    obj = storage.get("Amenity", amenity_id)
-    if obj is None:
-        abort(404)
-
-    s = request.get_json(silent=True)
-    if s is None:
-        abort(400, "Not a JSON")
-    else:
-        for k, v in s.items():
-            if k in ['id', 'created_at', 'updated_at']:
-                pass
-            else:
-                setattr(obj, k, v)
-        storage.save()
-        res = obj.to_dict()
-        return jsonify(res), 200
+    if request.method == 'PUT':
+        req_json = request.get_json()
+        if req_json is None:
+            abort(400, 'Not a JSON')
+        amenity_obj.bm_update(req_json)
+        return jsonify(amenity_obj.to_json()), 200
