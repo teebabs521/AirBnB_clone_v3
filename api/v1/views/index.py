c3eb89c0b727e268ec510830b93ac146da2f867d
Ibrahim Tijani <teebabs52@gmail.com>
2025-02-17 10:31:43 +0000

    Updates

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,3 +9,27 @@ def status():
     """Returns JSON status"""
     return jsonify({"status": "OK"})
 
+#!/usr/bin/python3
+"""Index route for API"""
+
+from api.v1.views import app_views
+from flask import jsonify
+from models import storage
+
+@app_views.route('/status', methods=['GET'])
+def status():
+    """Returns JSON status"""
+    return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    """Retrieves the number of each object by type"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    })
+
